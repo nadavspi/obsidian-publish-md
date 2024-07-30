f53ecdf7923c5669d8de26bb4ca5e76322963b15
Nadav Spiegelman <me@nadav.name>
2024-07-30 11:39:18 -0400

    Make jest work with esm

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
+	extensionsToTreatAsEsm: [".ts"],
+	moduleNameMapper: {
+		"^(\\.{1,2}/.*)\\.js$": "$1",
+	},
+	transform: {
+		// '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
+		// '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
+		"^.+\\.tsx?$": [
+			"ts-jest",
+			{
+				useESM: true,
+			},
+		],
+	},
 };
