64980ea1014cb4cfe037632bea91679cf22f998e
Nadav Spiegelman <me@nadav.name>
2024-07-31 01:02:38 -0400

    Copy images

--- src/process.ts
+++ src/process.ts
@@ -1,7 +1,9 @@
 import * as fs from "node:fs/promises";
+import { basename as pathBasename } from "node:path";
+import { Notice } from "obsidian";
 import slugify from "slugify";
 import { parse as parseYaml } from "yaml";
-import Publish from "../main";
+import type Publish from "../main";
 import pipe from "./pipe";
 import rewriteImages from "./rewriteImages";
 import type { PublishSettings } from "./types";
@@ -46,16 +48,14 @@ export default async function process({
 		removeNotes,
 		rewriteImages,
 	)({ content, slug, settings });
-	const outputFile = [
-		settings.outputPath,
-		frontmatter.type || defaultSubdir,
-		`${slug}.mdx`,
-	]
+	const outputDir = [settings.outputPath, frontmatter.type || defaultSubdir]
 		.filter(Boolean)
 		.join("/");
+	const outputFile = [outputDir, `${slug}.mdx`].filter(Boolean).join("/");
 
-	const images = await getImages({ settings, plugin });
+	const images = new ImageProcessor({ settings, plugin, outputDir, slug });
 
+	await fs.mkdir(outputDir, { recursive: true });
 	await fs.writeFile(outputFile, outputContent);
 	return outputFile;
 }
@@ -83,23 +83,75 @@ const stripWikilinks = (params: ProcessorParams): ProcessorParams => {
 	};
 };
 
-const getImages = ({
-	settings,
-	plugin,
-}: { settings: PublishSettings; plugin: Publish }): string[] => {
-	const file = plugin.app.workspace.getActiveFile()?.path;
-	if (!file) {
-		throw new Error("No active file");
+interface ImageFile {
+	path: string;
+	arrayBuffer: Promise<ArrayBuffer>;
+}
+
+class ImageProcessor {
+	outputDir: string;
+	slug: string;
+
+	constructor({
+		settings,
+		plugin,
+		outputDir,
+		slug,
+	}: {
+		settings: PublishSettings;
+		plugin: Publish;
+		outputDir: string;
+		slug: string;
+	}) {
+		this.outputDir = outputDir;
+		this.slug = slug;
+
+		const images = this.get({ settings, plugin });
+		this.copy(images);
 	}
-	const links: string[] = Object.keys(
-		plugin.app.metadataCache.resolvedLinks[
-			// @ts-ignore we checked that there's a file
-			plugin.app.workspace.getActiveFile().path
-		],
-	);
-	const images = links.filter((link) =>
-		settings.imageFileExtensions.some((ext) => link.endsWith(ext)),
-	);
-	console.log({ links, images });
-	return images;
-};
+	get({
+		settings,
+		plugin,
+	}: {
+		settings: PublishSettings;
+		plugin: Publish;
+	}): ImageFile[] {
+		const file = plugin.app.workspace.getActiveFile()?.path;
+		if (!file) {
+			throw new Error("No active file");
+		}
+		const links: string[] = Object.keys(
+			plugin.app.metadataCache.resolvedLinks[
+				// @ts-ignore we checked that there's a file
+				plugin.app.workspace.getActiveFile().path
+			],
+		);
+		const images = links
+			.filter((link) =>
+				settings.imageFileExtensions.some((ext) => link.endsWith(ext)),
+			)
+			.map((path) => {
+				const file = plugin.app.vault.getFileByPath(path);
+				if (!file) {
+					throw new Error(`Couldn't get the file ${path}`);
+				}
+				const arrayBuffer = plugin.app.vault.readBinary(file);
+				return { path, arrayBuffer };
+			});
+		return images;
+	}
+
+	async copy(images: ImageFile[]) {
+		for (const source of images) {
+			try {
+				const dir = `${this.outputDir}/${this.slug}`;
+				await fs.mkdir(dir, { recursive: true });
+				const buffer = Buffer.from(await source.arrayBuffer);
+				const file = `${dir}/${pathBasename(source.path)}`;
+				await fs.writeFile(file, buffer);
+			} catch (error: unknown) {
+				new Notice(`${error}`);
+			}
+		}
+	}
+}
