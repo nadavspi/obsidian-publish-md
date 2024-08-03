5c37c867050d7743cbdae5a1687d8f5414a54493
Nadav Spiegelman <me@nadav.name>
2024-08-02 22:32:07 -0400

    Remove default outputPath value

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 import {
-  Notice,
-  Plugin,
-  PluginSettingTab,
-  Setting,
-  type App,
-  type Editor
+    Notice,
+    Plugin,
+    PluginSettingTab,
+    Setting,
+    type App,
+    type Editor
 } from "obsidian";
 import process from "./src/process";
 import type { PublishSettings } from "./src/types";
 
 const DEFAULT_SETTINGS: PublishSettings = {
-	outputPath: "~/src/nadav.is/src/content/media/",
+	outputPath: "",
 	defaultSubdir: "",
 	imageFileExtensions: ["jpg", "png", "jpeg", "webp"],
 };
@@ -26,6 +26,10 @@ export default class Publish extends Plugin {
 			name: "Publish current file",
 			editorCallback: async (editor: Editor) => {
 				try {
+					if (!this.settings.outputPath) {
+						throw new Error("Output path must be configured in settings");
+					}
+
 					const content = editor.getValue();
 					const basename = this.app.workspace.getActiveFile()?.basename || "";
 					const output = await process({
